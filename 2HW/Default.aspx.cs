8f7d1260c45a6a6ca42074dd5ea176a33fbe2b8a
君蘋 魏 <107111205@mail.oit.edu.tw>
2021-03-30 13:15:56 +0800

    作業2

--- 2HW/Default.aspx.cs
+++ 2HW/Default.aspx.cs
@@ -8,7 +8,23 @@ using System.Web.UI.WebControls;
 namespace _2HW {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
-        }
+			int N = 15;
+			int sum = 0;
+			for (int x = 1; x <= N; x++)
+			{
+				if ((x % 2 == 0) || (x % 3 == 0))
+				{
+					if ((x % 2 == 0) && (x % 3 == 0))
+					{
+						sum += x;
+					}
+					else
+					{
+						sum += x;
+					}
+				}
+			}
+			Response.Write(sum);
+		}
     }
 }
